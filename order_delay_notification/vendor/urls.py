eaf372488c51e6a6b30595486d4d9723d32c5903
tahooramajlesi <tahooramajlesi@yahoo.com>
2023-07-27 15:56:25 +0330

    update docker-compose.yml

--- order_delay_notification/vendor/urls.py
+++ order_delay_notification/vendor/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("delay_order", views.delay_order),
-    path("assign_order_to_employee/<order_id>", views.assign_order_to_agent, name="order_id"),
+    path("assign_order_to_agent/<order_id>", views.assign_order_to_agent, name="order_id"),
     path("recieve_delay_report", views.recieve_store_delay_reports)
 ]
